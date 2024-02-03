e0975aa1597a395cf50fa7c829c2f9cd822c1972
Hansajaa <nilanahansaja21@gmail.com>
2024-02-03 16:41:08 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { SideMenuComponent } from './common/side-menu/side-menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentRegistrationComponent } from './pages/student-registration/student-registration.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    HomeComponent,
+    StudentRegistrationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
